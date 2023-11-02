56d343d14748b30d7ef931e26eefc83bd58cb04f
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-11-02 21:35:11 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+root_directory="/path/to/your/root/directory"
+
+if [ -d "$root_directory" ]; then
+  cd "$root_directory"
+  for dir in */; do
+    if [ -d "$dir" ]; then
+      cd "$dir"
+      touch example.txt
+      cd ..
+    fi
+  done
+else
+  echo "Thư mục gốc không tồn tại."
+fi
 
