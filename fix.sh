50b1fa8aae074bbe0667b1f0a46955657ba211a1
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-11-02 21:43:04 +0700

    Update and rename a.sh to fix.sh

--- fix.sh
+++ fix.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 root_directory="/www/wwwroot"
 
 if [ -d "$root_directory" ]; then
@@ -7,11 +5,13 @@ if [ -d "$root_directory" ]; then
   for dir in */; do
     if [ -d "$dir" ]; then
       cd "$dir"
-      touch example.txt
-      cd ..
+      cd public/theme/v2board/assets
+      rm -rf compoments.js
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/compoments.js
+      chown www:www compoments.js
+      cd "$root_directory"
     fi
   done
 else
   echo "Thư mục gốc không tồn tại."
 fi
-
