73ff42b1c59778660982c4721a13e2d2994ab17f
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-12-02 23:22:44 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -5,10 +5,11 @@ if [ -d "$root_directory" ]; then
   for dir in */; do
     if [ -d "$dir" ]; then
       cd "$dir"
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/config.zip
-      unzip config.zip
-      rm -rf config.zip
-      chown www:www config
+      rm -rf appconvert
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/appconvert.zip
+      unzip appconvert.zip
+      rm -rf appconvert.zip
+      chown www:www appconvert
       cd "$root_directory"
     fi
   done
