a6e8a65f73549b60b7f2c94d0ad668f576ec7095
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-12-04 05:01:21 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -5,10 +5,10 @@ if [ -d "$root_directory" ]; then
   for dir in */; do
     if [ -d "$dir" ]; then
       cd "$dir"
-      cd api
-      rm -rf confirmOrder.php
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/confirmOrder.php
-      chown www:www confirmOrder.php
+      cd core
+      rm -rf helpers.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/helpers.php
+      chown www:www helpers.php
       cd "$root_directory"
     fi
   done
