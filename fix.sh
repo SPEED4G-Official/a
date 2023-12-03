64d78a82409815a162a6ea34ab6800d0be559348
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-12-04 04:53:34 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -5,10 +5,10 @@ if [ -d "$root_directory" ]; then
   for dir in */; do
     if [ -d "$dir" ]; then
       cd "$dir"
-      cd giaodien1/views/client
-      rm -rf shareplan.php
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/shareplan.php
-      chown www:www shareplan.php
+      cd ajaxs/client
+      rm -rf create_order.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/create_order.php
+      chown www:www create_order.php
       cd "$root_directory"
     fi
   done
