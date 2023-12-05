8adba002eada1f515aca8f433b8a780cd21ad4df
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-12-06 05:08:01 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -9,6 +9,18 @@ if [ -d "$root_directory" ]; then
       rm -rf thanhtoan.php
       curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/thanhtoan.php
       chown www:www thanhtoan.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/acb.php
+      chown www:www acb.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/mbbank.php
+      chown www:www mbbank.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/momo.php
+      chown www:www momo.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/vietcombank.php
+      chown www:www vietcombank.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/viettelpay.php
+      chown www:www viettelpay.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/viettinbank.php
+      chown www:www viettinbank.php
       cd "$root_directory"
     fi
   done
