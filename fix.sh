e71e69ec0c14d054c74470ce91e4b530a032f279
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-12-05 23:37:39 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -6,9 +6,9 @@ if [ -d "$root_directory" ]; then
     if [ -d "$dir" ]; then
       cd "$dir"
       cd giaodien1/views/client
-      rm -rf order.php
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/order.php
-      chown www:www order.php
+      rm -rf orders.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/orders.php
+      chown www:www orders.php
       cd "$root_directory"
     fi
   done
