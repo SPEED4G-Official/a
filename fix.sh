36b897789c53b6545c8bbfe07cb33d5dde64378f
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-12-05 17:31:12 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -6,9 +6,9 @@ if [ -d "$root_directory" ]; then
     if [ -d "$dir" ]; then
       cd "$dir"
       cd giaodien1/views/client
-      rm -rf addshareplan.php
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/addshareplan.php
-      chown www:www addshareplan.php
+      rm -rf editshareplan.php
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/editshareplan.php
+      chown www:www editshareplan.php
       cd "$root_directory"
     fi
   done
