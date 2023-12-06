21f91128ec85ad2efd3c7ca856c2c403e8ba7e30
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-12-06 23:13:32 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -7,18 +7,8 @@ if [ -d "$root_directory" ]; then
       cd "$dir"
       cd giaodien1/views/admin
       rm -rf user.php
-      rm -rf plan-success.php
       curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/user.php
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/plan-success.php
       chown www:www user.php
-      chown www:www plan-success.php
-      cd "$root_directory"
-      cd "$dir"
-      cd ajaxs/admin
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/removeNoPlan.php
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/removePlanExpired.php
-      chown www:www removeNoPlan.php
-      chown www:www removePlanExpired.php
       cd "$root_directory"
     fi
   done
