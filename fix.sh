6378a8ddf54d14ead7dcf1925ccb2304d537f10c
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2023-12-10 02:38:27 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -5,10 +5,9 @@ if [ -d "$root_directory" ]; then
   for dir in */; do
     if [ -d "$dir" ]; then
       cd "$dir"
-      cd giaodien1/views/client
-      rm -rf page-end.php
-      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/page-end.php
-      chown www:www page-end.php
+      cd ajaxs/admin
+      curl -OL https://raw.githubusercontent.com/SPEED4G-Official/a/main/loadPrices.php
+      chown www:www loadPrices.php
       cd "$root_directory"
     fi
   done
