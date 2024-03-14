6b036d20188490aa718a0a7608e14b2941608fdf
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2024-03-14 16:19:06 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-while :
-do
-    result=$(telnet 103.129.126.180 7800 2>&1 | grep "Connection refused")
-
-    if [[ -n $result ]]; then
-        echo "Connection refused detected. Restarting service..."
-        service bt restart
-    else
-        echo "Connection is successful."
-    fi
-    sleep 1
-done
