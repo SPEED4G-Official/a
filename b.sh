57b9409175c1585b41582882275f020530ce5af3
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2024-03-14 16:11:45 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,14 @@
-if core-server status | grep -q "level=error"; then
-    core-server restart
-    echo "Khởi Động Lại Thành Công"
-else
-    echo "Không Tìm Thấy Lỗi"
-fi
+#!/bin/bash
+
+while :
+do
+    result=$(telnet 103.69.96.101 7801 2>&1 | grep "Connection refused")
+
+    if [[ -n $result ]]; then
+        echo "Connection refused detected. Restarting service..."
+        service bt restart
+    else
+        echo "Connection is successful."
+    fi
+    sleep 1
+done
